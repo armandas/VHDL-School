47d2042d046ad6bfa7931300003eb513e7499350
Armandas Jarusauskas <jarusauskas@gmail.com>
2009-07-07 12:41:47 +0100

    Changing port names

--- BinaryLED/bin_led.vhd
+++ BinaryLED/bin_led.vhd
@@ -3,21 +3,20 @@ use ieee.std_logic_1164.all;
 
 entity bin_led is
     port (
-        bin: in std_logic_vector(3 downto 0);
-        led: out std_logic_vector(7 downto 0)
+        binary: in std_logic_vector(2 downto 0);
+        unary: out std_logic_vector(7 downto 0)
     );
 end bin_led;
 
 architecture bin_led_arch of bin_led is
 begin
-    with bin select
-        led <= "00000000" when "0000",
-               "00000001" when "0001",
-               "00000011" when "0010",
-               "00000111" when "0011",
-               "00001111" when "0100",
-               "00011111" when "0101",
-               "00111111" when "0110",
-               "01111111" when "0111",
-               "11111111" when others;
+    with binary select
+        unary <= "00000000" when "000",
+                 "00000001" when "001",
+                 "00000011" when "010",
+                 "00000111" when "011",
+                 "00001111" when "100",
+                 "00011111" when "101",
+                 "00111111" when "110",
+                 "01111111" when others;
 end bin_led_arch;
