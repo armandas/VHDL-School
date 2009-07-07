b739f12dc39068dde272427f6d393b2b4d9ac9b2
Armandas Jarusauskas <jarusauskas@gmail.com>
2009-07-07 13:08:42 +0100

    Makes use of bin_led entity. Some refactoring done as well.

--- BinaryCounter/bin_count.vhd
+++ BinaryCounter/bin_count.vhd
@@ -4,23 +4,29 @@ use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
 entity bin_count is
-    generic(N: integer := 4);
+    generic(N: integer := 3);
     port (
-        clk: in std_logic;
-        led: out std_logic_vector(N-1 downto 0)
+        clk, reset: in std_logic;
+        count: out std_logic_vector((2 ** N) - 1 downto 0)
     );
 end bin_count;
 
 architecture bin_count_arch of bin_count is
-    signal val: std_logic_vector(N-1 downto 0);
+    signal bin: std_logic_vector(N - 1 downto 0);
+    signal val: std_logic_vector((2 ** N) - 1 downto 0);
 begin
-    process(clk)
+    bled: entity work.bin_led(bin_led_arch)
+        port map(binary => bin, unary => val);
+
+    process(clk, reset)
     begin
-        if (clk'event and clk = '0') then
-            val <= val + 1;
+        if (reset = '1') then
+            bin <= (others => '0');
+        elsif (clk'event and clk = '0') then
+            bin <= bin + 1;
         end if;
     end process;
     
-    led <= val;
+    count <= val;
 end bin_count_arch;
 
