803f9175227e178e4f87b83e53b4c60b1f8df9f6
Armandas Jarusauskas <jarusauskas@gmail.com>
2009-07-16 13:23:17 +0100

    Some progress with the task.
    Crrently there is a problem with the square being unable to get to
    the top of the display.

--- SquareThingy/rotate.vhd
+++ SquareThingy/rotate.vhd
@@ -2,6 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
+
 entity circle is
     port(
         clk: in std_logic;
@@ -12,32 +13,46 @@ entity circle is
 end circle;
 
 architecture rotate of circle is
-    constant M: integer := 28;
-    signal disp_next: std_logic_vector(3 downto 0);
-    signal counter: std_logic_vector(M-1 downto 0);
-    signal sel: std_logic_vector(1 downto 0);
-    signal top: std_logic;
+    constant M: integer := 23;
+    signal clock_divider: std_logic_vector(M-1 downto 0);
+    signal counter, counter_next: std_logic_vector(1 downto 0);
+    signal top, top_next: std_logic;
 begin
 
-    process(clk, counter)
+    process(clk, clock_divider)
     begin
         if (clk'event and clk = '0') then
-            counter <= counter + '1';
-            disp <= disp_next;
+            clock_divider <= clock_divider + '1';
+            if (clock_divider = 0) then
+                counter <= counter_next;
+                top <= top_next;
+            end if;
         end if;
     end process;
-
-    top <= '0' when (cw = '1' and disp_next = "1110") or
-                    (cw = '0' and disp_next = "0111") else
-           '1' when (cw = '1' and disp_next = "0111") or
-                    (cw = '0' and disp_next = "1110");
+    
+    -- Square needs to stay on the same display twice when at the ends.
+    counter_next <= counter when ((counter = "0111" and top /= cw) or
+                                  (counter = "1110" and top = cw)) else
+                    counter - '1' when (top = cw) else
+                    counter + '1';
+                    
     value <= "01100011" when (top = '1') else "00011101";
+    
+    top_next <= not top when (counter = counter_next);
+        
+    process(counter)
+    begin
+        case counter is
+            when "00" =>
+                disp <= "1110";
+            when "01" =>
+                disp <= "1101";
+            when "10" =>
+                disp <= "1011";
+            when others =>
+                disp <= "0111";
+        end case;
+    end process;
 
-    sel <= counter(M-1 downto M-2);
-    with sel select
-        disp_next <= "1110" when "00",
-                     "1101" when "01",
-                     "1011" when "10",
-                     "0111" when others;
 end rotate;
 
