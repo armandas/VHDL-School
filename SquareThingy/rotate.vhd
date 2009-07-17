96f63bf84dda452473d4d57761f5918b519f0284
Armandas Jarusauskas <jarusauskas@gmail.com>
2009-07-17 13:29:35 +0100

    Some updates. Nothing works. Still.

--- SquareThingy/rotate.vhd
+++ SquareThingy/rotate.vhd
@@ -14,6 +14,7 @@ end circle;
 
 architecture rotate of circle is
     constant M: integer := 23;
+    signal disp_current, disp_next: std_logic_vector(3 downto 0);
     signal clock_divider: std_logic_vector(M-1 downto 0);
     signal counter, counter_next: std_logic_vector(1 downto 0);
     signal top, top_next: std_logic;
@@ -25,32 +26,35 @@ begin
             clock_divider <= clock_divider + '1';
             if (clock_divider = 0) then
                 counter <= counter_next;
+                disp <= disp_next;
+                disp_current <= disp_next;
                 top <= top_next;
             end if;
         end if;
     end process;
     
     -- Square needs to stay on the same display twice when at the ends.
-    counter_next <= counter when ((counter = "0111" and top /= cw) or
-                                  (counter = "1110" and top = cw)) else
+    counter_next <= counter when (disp_current = disp_next and
+                                  ((disp_next = "0111" and top /= cw) or
+                                   (disp_next = "1110"  and top = cw))) else
                     counter - '1' when (top = cw) else
                     counter + '1';
                     
     value <= "01100011" when (top = '1') else "00011101";
     
-    top_next <= not top when (counter = counter_next);
-        
+    top_next <= (not top) when (counter = counter_next);
+
     process(counter)
     begin
         case counter is
             when "00" =>
-                disp <= "1110";
+                disp_next <= "1110";
             when "01" =>
-                disp <= "1101";
+                disp_next <= "1101";
             when "10" =>
-                disp <= "1011";
+                disp_next <= "1011";
             when others =>
-                disp <= "0111";
+                disp_next <= "0111";
         end case;
     end process;
 
