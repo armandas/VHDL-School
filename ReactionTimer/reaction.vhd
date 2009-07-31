3b15f4bf14092e8364c07a8fda411f5ab166f5d1
Armandas Jarusauskas <jarusauskas@gmail.com>
2009-07-31 11:02:17 +0100

    Commiting changes for possible branching.
    At this point, a branch may be needed since I'm not sure which method
    would be the best for counting milliseconds.

--- ReactionTimer/reaction.vhd
+++ ReactionTimer/reaction.vhd
@@ -4,98 +4,133 @@ use ieee.std_logic_unsigned.all;
 
 entity reaction is
     port(
-        clk: in std_logic;
-        start, stop, clear: in std_logic
+        clk, reset: in std_logic;
+        start, stop: in std_logic;
+        sseg_en: out std_logic_vector(3 downto 0);
+        sseg: out std_logic_vector(7 downto 0);
+        led: out std_logic
     );
 end reaction;
 
 architecture measure of reaction is
     constant MILLISECOND: integer := 50000;
-    
     type state_type is (idle, rand_wait, count);
-    type sseg_msg is array(3 downto 0) of std_logic_vector(7 downto 0);
 
     signal state, state_next: state_type;
-    signal message: sseg_msg;
+    signal m3, m2, m1, m0: std_logic_vector(7 downto 0);
     signal timer, timer_next: std_logic_vector(15 downto 0);
     -- t will be increased when timer reaches 50000 (i.e. 1ms at 50MHz)
     signal t, t_next: std_logic_vector(9 downto 0);
     signal lol, fail: std_logic;
 begin
-    
-    process(clk)
+
+    process(clk, reset)
     begin
-        if clk'event and clk = '0' then
+        if reset = '1' then
+            state <= idle;
+        elsif (clk'event and clk = '0') then
             state <= state_next;
             timer <= timer_next;
             t <= t_next;
         end if;
     end process;
 
-    process(start, stop, clear, state)
+    process(start, stop, state, timer, t, lol, fail)
     begin
         case state is
             when idle =>
                 if start = '1' then
                     state_next <= rand_wait;
+                    timer_next <= (others => '0');
+                    t_next <= (others => '0');
                     -- clear stuff
-                    message <= (others => (others => '0'));
+                    m3 <= (others => '0');
+                    m2 <= (others => '0');
+                    m1 <= (others => '0');
+                    m0 <= (others => '0');
                     fail <= '0';
                     lol <= '0';
-                    t_next <= (others => '0');
+                    led <= '0';
                 else
-                    if t < 999 then
-                        message(3) <= "11111110"; -- 0.
-                        -- need to implement binary-to-bcd converter
-                    elsif lol = '1' then
-                        message(3) <= "00001110"; -- L
-                        message(2) <= "01111110"; -- O
-                        message(1) <= "00001110"; -- L
-                        message(0) <= (others => '0'); -- off
+                    state_next <= idle;
+                    if lol = '1' then
+                        m3 <= "00001110"; -- L
+                        m2 <= "01111110"; -- O
+                        m1 <= "00001110"; -- L
+                        m0 <= (others => '0'); -- off
                     elsif fail = '1' then
-                        message(3) <= "01000111"; -- F
-                        message(2) <= "01110111"; -- A
-                        message(1) <= "00000110"; -- I
-                        message(0) <= "00001110"; -- L
+                        m3 <= "01000111"; -- F
+                        m2 <= "01110111"; -- A
+                        m1 <= "00000110"; -- I
+                        m0 <= "00001110"; -- L
+                    elsif t > 0 then --and t < 999 then
+                        -- need to implement binary-to-bcd converter
+                        m3 <= "01011011"; -- S
+                        m2 <= "00010111"; -- h
+                        m1 <= "00000100"; -- i
+                        m0 <= "00001111"; -- t
                     else
-                        message(3) <= "00110111"; -- H
-                        message(2) <= "00000110"; -- I
-                        message(1) <= (others => '0'); -- off
-                        message(0) <= (others => '0'); -- off
+                        m3 <= "01001110"; -- C
+                        m2 <= "00011100"; -- u
+                        m1 <= "00010101"; -- n
+                        m0 <= "00001111"; -- t
+                        --m3 <= "00110111"; -- H
+                        --m2 <= "00000110"; -- I
+                        --m1 <= (others => '0'); -- off
+                        --m0 <= (others => '0'); -- off
                     end if;
                 end if;
 
             when rand_wait =>
-                if stop = '1' then
-                    fail <= '1';
-                    state_next <= idle;
-                 else
+                m3 <= "00110111"; -- H
+                m2 <= "00011101"; -- o
+                m1 <= "00110111"; -- H
+                m0 <= "00011101"; -- o
+                --if --stop = '1' then
+                    --fail <= '1';
+                    --state_next <= idle;
+                --else
                     -- random waiting happens here
                     --
                     -- if already_random then
                     --     state_next <= count;
                     -- end if;
-                 end if;
+                    state_next <= count;
+                --end if;
 
             when count =>
+                state_next <= count;
+                m3 <= "00110111"; -- H
+                m2 <= "00011101"; -- o
+                m1 <= "00110111"; -- H
+                m0 <= "00011101"; -- o
+                led <= '1';
                 if stop = '1' then
                     state_next <= idle;
                 else
-                    if timer = MILLISECOND - 1 then
-                        t_next <= t + 1;
-                        timer_next <= (others => '0');
-                    elsif t = 999 then
-                        lol <= '1';
-                        state_next <= idle;
+                    if timer = (MILLISECOND - 1) then
+                        if t = 999 then
+                            lol <= '1';
+                            state_next <= idle;
+                        else
+                            t_next <= t + 1;
+                            timer_next <= (others => '0');
+                        end if;
                     else
                         timer_next <= timer + 1;
                     end if;
                 end if;
         end case;
-    
+
     end process;
 
-    --sseg:display(message);
+    disp: entity work.sseg_mux(mux_arch)
+        port map(
+            clk => clk, reset => reset,
+            in0 => m0, in1 => m1, in2 => m2, in3 => m3,
+            en => sseg_en,
+            sseg => sseg
+        );
 
 end measure;
 
